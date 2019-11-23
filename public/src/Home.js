0318de7e29e3a180734fcf18c4ad6c78631f83a1
Chander Ramesh <hardly.hacking@gmail.com>
2019-11-23 14:43:36 -0800

    Make Home a class and add firebase web sdk to fetch firestore

--- public/src/Home.js
+++ public/src/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-function Home() {
-    return (
-        <div>Hello World</div>
-    );
+class Home extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    getLatestDonations() {
+        fetch()
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Reward content the way you see fit.</h2>
+            </div>
+        )
+    }
 }
 
 export default Home;
