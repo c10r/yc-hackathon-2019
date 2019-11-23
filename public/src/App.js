b4140f1ba5336ad24c74e1af4b6365764ec97ef6
Tushar Dhoot <tushardhoot@Tushars-MacBook-Air.local>
2019-11-23 14:14:53 -0800

    react router and nav

--- public/src/App.js
+++ public/src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route
+} from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Home from './Home';
+import Trending from './Trending';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div className="App">
+          <Navbar bg="primary" variant="dark" expand="lg">
+            <Navbar.Brand href="/">Credz.io</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Nav className="mr-auto" navbar>
+              <Nav.Link tag={Link} href="/trending">Trending</Nav.Link>
+            </Nav>
+          </Navbar>
+          <div className="content">
+            <Route path="/" component={Home}/>
+            <Route path="/trending" component={Trending}/>
+          </div>
+      </div>
+    </Router>
   );
 }
 
